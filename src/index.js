1d288d77f25c1e527eade4b1dace9a31d2a02467
stevesdiary <94493468+stevesdiary@users.noreply.github.com>
2024-04-25 23:59:39 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// import express from "express";
+// import http from "http";
+// import bodyParser from "body-parser";
+// import cookieParser from "cookie-parser";
+// import compression from "compression";
+// import cors from "cors";
+// import router from './router';
+// import mongoose, { mongo } from "mongoose";
+// const app = express();
+var employee = {
+    id: 1,
+    name: 'Sam',
+    retire: function (date) {
+        console.log(date);
+    }
+};
+// console.log(employee.name)
