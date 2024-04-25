4d30ca729fa295a33e6e3148de8c88c2555e5da5
stevesdiary <94493468+stevesdiary@users.noreply.github.com>
2024-04-25 23:59:44 +0100

    Update users.ts

--- src/controller/users.ts
+++ src/controller/users.ts
@@ -1,48 +1,48 @@
-import express from 'express';
+// import express from 'express';
 
-import { deleteUserById, getUsers, getUserById } from '../db/users';
+// import { deleteUserById, getUsers, getUserById } from '../db/users';
 
-export const getAllUsers = async (req: express.Request, res: express.Response) => {
-  try {
-    const users = await getUsers();
+// export const getAllUsers = async (req: express.Request, res: express.Response) => {
+//   try {
+//     const users = await getUsers();
 
-    return res.status(200).json(users);
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
-  }
-};
+//     return res.status(200).json(users);
+//   } catch (error) {
+//     console.log(error);
+//     return res.sendStatus(400);
+//   }
+// };
 
-export const deleteUser = async (req: express.Request, res: express.Response) => {
-  try {
-    const { id } = req.params;
+// export const deleteUser = async (req: express.Request, res: express.Response) => {
+//   try {
+//     const { id } = req.params;
 
-    const deletedUser = await deleteUserById(id);
+//     const deletedUser = await deleteUserById(id);
 
-    return res.json(deletedUser);
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
-  }
-}
+//     return res.json(deletedUser);
+//   } catch (error) {
+//     console.log(error);
+//     return res.sendStatus(400);
+//   }
+// }
 
-export const updateUser = async (req: express.Request, res: express.Response) => {
-  try {
-    const { id } = req.params;
-    const { username } = req.body;
+// export const updateUser = async (req: express.Request, res: express.Response) => {
+//   try {
+//     const { id } = req.params;
+//     const { username } = req.body;
 
-    if (!username) {
-      return res.sendStatus(400);
-    }
+//     if (!username) {
+//       return res.sendStatus(400);
+//     }
 
-    const user = await getUserById(id);
+//     const user = await getUserById(id);
     
-    user.username = username;
-    await user.save();
+//     user.username = username;
+//     await user.save();
 
-    return res.status(200).json(user).end();
-  } catch (error) {
-    console.log(error);
-    return res.sendStatus(400);
-  }
-}
+//     return res.status(200).json(user).end();
+//   } catch (error) {
+//     console.log(error);
+//     return res.sendStatus(400);
+//   }
+// }
